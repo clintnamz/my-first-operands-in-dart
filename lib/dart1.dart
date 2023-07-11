3b05e6f6fc89a03390698a178184a73dc1696862
birungi <katabandamaclintongmail.com>
2023-07-10 22:49:58 -0700

    my first commit

---
+++ lib/dart1.dart
@@ -0,0 +1,65 @@
+// // int calculate() {
+// //   return 6 * 7;
+// // }
+// void main()
+// {
+// 	int a = 2;
+// 	int b = 3;
+
+// 	// Adding a and b
+// 	var c = a + b;
+// 	print("Sum of a and b is $c");
+
+// 	// Subtracting a and b
+// 	var d = a - b;
+// 	print("The difference between a and b is $d");
+
+// 	// Using unary minus
+// 	var e = -d;
+// 	print("The negation of difference between a and b is $e");
+
+// 	// Multiplication of a and b
+// 	var f = a * b;
+// 	print("The product of a and b is $f");
+
+// 	// Division of a and b
+// 	var g = b / a;
+// 	print("The quotient of a and b is $g");
+
+// 	// Using ~/ to divide a and b
+// 	var h = b ~/ a;
+// 	print("The quotient of a and b is $h");
+
+// 	// Remainder of a and b
+// 	var i = b % a;
+// 	print("The remainder of a and b is $i");
+// }
+
+void main() {
+  int a = 2;
+  int b = 3;
+
+  // Greater between a and b
+  var c = a > b;
+  print("a is greater than b is $c");
+
+  // Smaller between a and b
+  var d = a < b;
+  print("a is smaller than b is $d");
+
+  // Greater than or equal to between a and b
+  var e = a >= b;
+  print("a is greater than b is $e");
+
+  // Less than or equal to between a and b
+  var f = a <= b;
+  print("a is smaller than b is $f");
+
+  // Equality between a and b
+  var g = b == a;
+  print("a and b are equal is $g");
+
+  // Unequality between a and b
+  var h = b != a;
+  print("a and b are not equal is $h");
+}
